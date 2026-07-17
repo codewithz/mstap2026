471c3248db243d09b29bb4edd242a7d2c4749a18
Zartab M Nakhwa <zartab@codewithz.com>
2026-07-17 09:00:06 +0000

    List Sorting Exercise

--- python-playground/02-collections/list-sorting.py
+++ python-playground/02-collections/list-sorting.py
@@ -35,3 +35,47 @@ lambda item : item[1]
 
 cart.sort(key=lambda item: item[2])
 print("Sorted on Qty",cart)
+#---------------------------------------------------------------
+# -- filter
+# ---------------------------------------------------------------------
+
+# Filter those products whose price is greater than 5$
+
+cart=[('Bread',15,2),('Butter',
+                      7,3),('Eggs',3,1)]
+
+#  filtered_list=list(filter(funciton_where_filter_logic_is_written,source_collection))
+
+
+# def filter_price_above_5(cart_item):
+#     return cart_item[1]>5
+
+# lambda cart_item : cart_item[1]>5
+
+filtered_cart=list(filter(lambda cart_item : cart_item[1]>5,cart))
+
+print("Filtered Cart:",filtered_cart)
+
+print(30*"-")
+#---------------------------------------------------------------
+# -- map
+# ---------------------------------------------------------------------
+
+numbers=[1,2,3,4,5]
+
+# mapped_list=list(map(function_where_transformation_logic_is_written,source_collection))
+
+def square_of_the_number(number):
+    return pow(number,2)
+
+lambda number: pow(number,2)
+
+squared_numbers=list(map(square_of_the_number,numbers))
+
+print("O:",numbers)
+print("M:",squared_numbers)
+
+# -------------------------- Exercise ------------------
+# (Name,Price,Qty)  ---> (Name,Price,Qty,Total)
+cart=[('Bread',15,2),('Butter',7,3),('Eggs',3,1)]
+
