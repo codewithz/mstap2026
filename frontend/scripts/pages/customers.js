718fe9446b1cd84c871f47b010e03f2580d53d46
Zartab Nakhwa <zartab@codewithz.com>
2026-08-05 08:55:59 +0000

    Cpatured Data from Form

--- frontend/scripts/pages/customers.js
+++ frontend/scripts/pages/customers.js
@@ -9,3 +9,29 @@ async function loadAndRenderCustomers() {
 }
 
 await loadAndRenderCustomers();
+
+const form= document.querySelector("#customer-form");
+const nameField= document.querySelector("#name");
+const emailField= document.querySelector("#email");
+const phoneField= document.querySelector("#phone");
+const dobField= document.querySelector("#dob");
+const joinDateField= document.querySelector("#joinDate");
+const branchField= document.querySelector("#branch");
+const balanceField= document.querySelector("#balance");
+
+function readFormValue(){
+    return {
+        name:nameField.value,
+        email:emailField.value,
+        phone:phoneField.value,
+        dob:dobField.value,
+        joinDate:joinDateField.value,
+        branch:branchField.value,
+        balance:Number(balanceField.value) || 0
+    };
+}
+
+form.addEventListener('submit',(event)=>{
+        event.preventDefault();
+        console.log(readFormValue())
+});
